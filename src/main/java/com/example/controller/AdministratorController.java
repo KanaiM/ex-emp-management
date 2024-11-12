26c7f559c978e76e4a62d59b968e58fbdd95578c
minami <minami.kanai@rakus-partners.co.jp>
2024-11-12 17:10:32 +0900

    AdministratorController作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -3,11 +3,15 @@ package com.example.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.form.InsertAdministratorForm;
 import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +28,9 @@ public class AdministratorController {
 
     @Autowired
     private AdministratorService administratorService;
+
+    @Autowired
+    private HttpSession session;
 
     @ModelAttribute
     private InsertAdministratorForm setUpForm(){
@@ -46,5 +53,16 @@ public class AdministratorController {
     @GetMapping("/")
     public String toLogin(LoginForm form) {
         return "administrator/login";
+    }
+
+    @PostMapping("/login")
+    public String login(LoginForm form, Model model) {
+        Administrator date = administratorService.login(form.getMailAddress(), form.getPassword());
+        if(date == null){
+            model.addAttribute("message", "メールアドレスまたはパスワードが不正です。");
+            return "administrator/login";
+        }
+        session.setAttribute("administratorName", date);
+        return "redirect:/employee/showList";
     }
 }
