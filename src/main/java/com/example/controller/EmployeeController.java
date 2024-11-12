a3eea525d368fa5fc07c077bbfa7b8a66f4405cb
minami <minami.kanai@rakus-partners.co.jp>
2024-11-12 17:09:55 +0900

    EmployeeController作成

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+/**
+ * 従業員情報を検索する処理を記述する 
+ * @author kanaiminami
+ */
+@Controller
+public class EmployeeController {
+
+}
