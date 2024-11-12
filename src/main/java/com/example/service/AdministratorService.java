f2406e0fba4ff638ce9e0d3841f9e9d0dbe68ff9
minami <minami.kanai@rakus-partners.co.jp>
2024-11-12 17:07:55 +0900

    AdminstratorServiceフォーム作成

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 /**
- *  管理者情報を登録する業務処理を⾏うメソッドを作成する
+ *  管理者情報を操作するサービス 
  * @author kanaiminami
  */
 
@@ -17,11 +17,21 @@ public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository;
 
+    /**
+     * 管理者情報を登録する業務処理を⾏う
+     * @param administrator
+     */
     public void insert(Administrator administrator){
         administratorRepository.insert(administrator);
     }
-
-    public Administrator findByMailAddressAndPassword(String mailAddress, String password){
+    /**
+     * ログインをする業務処理を⾏う
+     * @param mailAddress
+     * @param password
+     * @return メールアドレスとパスワードから取得した管理者情報
+     */
+    public Administrator login(String mailAddress, String password){
         return administratorRepository.findByMailAddressAndPassword(mailAddress, password);
     }
+
 }
