2431d3bed56b591e6f11555daa8b7e7144f55786
minami <minami.kanai@rakus-partners.co.jp>
2024-11-12 14:19:09 +0900

    AdministratorController作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.domain.Administrator;
+
 /**
  * 管理者登録画⾯を表⽰する処理を記述する
  * @author kanaiminami
@@ -19,9 +23,24 @@ public class AdministratorController {
     @Autowired
     private AdministratorService administratorService;
 
+    @ModelAttribute
+    private InsertAdministratorForm setUpForm(){
+        return new InsertAdministratorForm();
+    }
+
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
+    }
+    
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator();
+        administrator.setName(form.getName());
+        administrator.setMailAddress(form.getMailAddress());
+        administrator.setPassword(form.getPassword());
+        administratorService.insert(administrator);
+        return "/";
     }
     
 }
