9ce281add5b775f18abb1c90fb0247c951ecea33
minami <minami.kanai@rakus-partners.co.jp>
2024-11-13 16:52:59 +0900

    入力値チェックを追加

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,11 +1,19 @@
 package com.example.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使⽤するフォーム 
  * @author kanaiminami
  */
 public class InsertAdministratorForm {
+    @NotBlank(message = "必須入力です")
     private String name;
+    @NotBlank(message = "必須入力です")
     private String mailAddress;
+    @NotBlank(message = "必須入力です")
+    @Size(min=3, max=127, message="3文字以上127文字以内で記載してください")
     private String password;
     
     public String getName() {
