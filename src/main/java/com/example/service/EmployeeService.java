f5ac5d4bc9f9770600d4aa118fe0552352845a1a
minami <minami.kanai@rakus-partners.co.jp>
2024-11-13 14:06:15 +0900

    EmployeeService作成

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -37,4 +37,12 @@ public class EmployeeService {
     public Employee showDetail(Integer id){
         return employeeRepository.load(id);
     }
+
+    /**
+     * 従業員情報を更新する
+     * @param employee
+     */
+    public void update(Employee employee){
+        employeeRepository.update(employee);
+    } 
 }
