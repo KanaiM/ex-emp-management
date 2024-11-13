71762febb474aab50a16d2341b1b6501e52f270b
minami <minami.kanai@rakus-partners.co.jp>
2024-11-13 11:17:12 +0900

    EmployeeController作成

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,11 +1,45 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Employee;
+import com.example.form.UpdateEmployeeForm;
+import com.example.service.EmployeeService;
 /**
  * 従業員情報を検索する処理を記述する 
  * @author kanaiminami
  */
+
 @Controller
+@RequestMapping("/employee")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * 従業員⼀覧を出⼒する
+     * @param model
+     * @return 全件取得した従業員情報
+     */
+    @GetMapping("/showList")
+    public String showList(Model model) {
+        List<Employee> employeeList = employeeService.showList();
+        model.addAttribute("emlpoyeeList", employeeList);
+        return "employee/list";
+    }
+    
+    @GetMapping("/showDetail")
+    public String showDetail(String id, Model model, UpdateEmployeeForm form){
+        Employee employee = employeeService.showDetail(Integer.parseInt(id));
+        model.addAttribute("employee", employee);
+        return "employee/detail";       
+    }
 }
