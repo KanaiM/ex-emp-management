e5edf9b79fe6179caeedea17447de4fc8dce5192
minami <minami.kanai@rakus-partners.co.jp>
2024-11-13 14:05:37 +0900

    EmployeeController作成

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -12,10 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.domain.Employee;
 import com.example.form.UpdateEmployeeForm;
 import com.example.service.EmployeeService;
+
+import jakarta.servlet.http.HttpSession;
+
 /**
  * 従業員情報を検索する処理を記述する 
  * @author kanaiminami
  */
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @RequestMapping("/employee")
@@ -23,6 +28,7 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
 
     /**
      * 従業員⼀覧を出⼒する
@@ -49,4 +55,20 @@ public class EmployeeController {
         model.addAttribute("employee", employee);
         return "employee/detail";       
     }
+
+    /**
+     * 扶養⼈数を更新する
+     * @param form
+     * @return 従業員⼀覧を出⼒する
+     */
+    @PostMapping("/update")
+    public String update(UpdateEmployeeForm form) {
+        System.out.println(form.getId());
+        employeeService.showDetail(Integer.parseInt(form.getId()));
+        Employee employee = new Employee();
+        employee.setDependentsCount(Integer.parseInt(form.getDependentsCount()));
+        employeeService.update(employee);
+        return "redirect:/employee/showList";
+    }
+    
 }
